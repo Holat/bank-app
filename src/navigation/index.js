3e1ce749dedd201a60306d4c8ecc148287b008f0
Holat <105239299+Holat@users.noreply.github.com>
2023-09-29 22:27:13 +0100

    tab nav added

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,16 +2,35 @@ import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen, WelcomeScreen } from "../screens";
+import { HomeScreen, WelcomeScreen, SendScreen, PayScreen } from "../screens";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BanknotesIcon,
+  HomeIcon,
+  PaperAirplaneIcon,
+} from "react-native-heroicons/outline";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ tabBarIcon: () => <HomeIcon color={"black"} /> }}
+      />
+      <Tab.Screen
+        name="Send"
+        component={SendScreen}
+        options={{ tabBarIcon: () => <PaperAirplaneIcon color={"black"} /> }}
+      />
+      <Tab.Screen
+        name="Pay"
+        component={PayScreen}
+        options={{ tabBarIcon: () => <BanknotesIcon color={"black"} /> }}
+      />
     </Tab.Navigator>
   );
 };
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,6 @@
 import HomeScreen from "./HomeScreen";
 import WelcomeScreen from "./WelcomeScreen";
+import SendScreen from "./SendScreen";
+import PayScreen from "./PayScreen";
 
-export { HomeScreen, WelcomeScreen };
+export { HomeScreen, WelcomeScreen, SendScreen, PayScreen };
