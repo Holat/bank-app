59e91d6424b513dc0b28168c5c919cb7e626292c
Holat <105239299+Holat@users.noreply.github.com>
2023-09-29 22:27:24 +0100

    demo

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+import { BanknotesIcon } from "react-native-heroicons/solid";
+
+const Accounts = [
+  {
+    name: "others",
+    balance: "10,000",
+    color: "#06D6A0",
+  },
+  {
+    name: "Savings",
+    balance: "50,000",
+    color: "#F72585",
+  },
+  {
+    name: "Current",
+    balance: "10,000",
+    color: "#001c55",
+  },
+];
+
+export default Accounts;
