4a40dc9536907effdb2b04f13727407a7c2fcd81
Holat <105239299+Holat@users.noreply.github.com>
2023-09-30 14:52:39 +0100

    fonts

--- App.js
+++ App.js
@@ -1,5 +1,32 @@
 import AppNavigation from "./src/navigation";
+import { View } from "react-native";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
-  return <AppNavigation />;
+  const [fontsLoaded] = useFonts({
+    "Roboto-Thin": require("./assets/fonts/Roboto-Thin.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    Mon: require("./assets/fonts/Montserrat-VariableFont_wght.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <AppNavigation />
+    </View>
+  );
 }
