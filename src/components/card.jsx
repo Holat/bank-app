662685edc7d5f9d724233e2bde113911fe997918
Holat <105239299+Holat@users.noreply.github.com>
2023-10-05 22:51:27 +0100

    card animation 2

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useCallback, useMemo, useState } from "react";
-import { Accounts, Icon } from "../constants";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Icon } from "../constants";
 import Animated, {
-  Extrapolate,
   FadeInDown,
   interpolate,
   runOnJS,
@@ -12,38 +11,38 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
-import { BlurView } from "expo-blur";
-
-const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
 
 const Card = () => {
-  const inputRange = [0, 60];
+  const Accounts = [
+    {
+      id: "a0",
+      name: "Current",
+      balance: "10,000",
+      color: "#001c55",
+    },
+    {
+      id: "a1",
+      name: "Savings",
+      balance: "50,000",
+      color: "#F72585",
+    },
+    {
+      id: "a2",
+      name: "others",
+      balance: "10,000",
+      color: "#06D6A0",
+    },
+  ];
   const [currentAcct, setCurrentAcct] = useState("a0");
   const translateY = useSharedValue(0);
+  const stackOrder = [3, 2, 1];
+  const inputRange = [0, 60];
 
-  const zIndex = useSharedValue(3);
-  const top = useSharedValue(40);
-  const opacity = useSharedValue(1);
-  const width = useSharedValue(100);
-
-  const zIndex1 = useSharedValue(2);
-  const top1 = useSharedValue(20);
-  const opacity1 = useSharedValue(0.9);
-  const width1 = useSharedValue(90);
-
-  const zIndex2 = useSharedValue(1);
-  const top2 = useSharedValue(0);
-  const opacity2 = useSharedValue(0.8);
-  const width2 = useSharedValue(80);
-
-  // const moveItemToFront = useCallback(() => {
-  //   const itemToMove = Accounts.pop();
-  //   Accounts.unshift(itemToMove);
-  //   console.log(itemToMove);
-
-  //   console.log(Accounts);
-  //   setCurrentAcct(Accounts[2].id);
-  // }, []);
+  const move = useEffect(() => {
+    Accounts.unshift(Accounts[2]);
+    Accounts.pop();
+    console.log(Accounts);
+  }, [Accounts]);
 
   const panGesture = useMemo(
     () =>
@@ -53,25 +52,8 @@ const Card = () => {
         })
         .onEnd((e) => {
           if (translateY.value >= 60) {
-            translateY.value = withSpring(0);
-            zIndex.value = withTiming(-3);
-            top.value = withSpring(0);
-            width.value = withSpring(80);
-            opacity.value = withTiming(0.8);
-
-            zIndex1.value = withTiming(3);
-            top1.value = withSpring(40);
-            width1.value = withSpring(100);
-            opacity1.value = withTiming(1);
-
-            zIndex2.value = withTiming(2);
-            top2.value = withSpring(20);
-            width2.value = withSpring(90);
-            opacity2.value = withTiming(0.9, undefined, (isFinished) => {
-              if (isFinished) {
-                console.log("finished");
-              }
-            });
+            // runOnJS(move)();
+            translateY.value = 0;
           } else {
             translateY.value = withSpring(0);
           }
@@ -79,38 +61,20 @@ const Card = () => {
     []
   );
 
-  const rStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateY: translateY.value }],
-      opacity: opacity.value,
-      width: `${width.value}%`,
-      top: top.value,
-      zIndex: zIndex.value,
-    };
-  });
+  const rStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }));
 
-  const rStyle1 = useAnimatedStyle(() => {
-    // const width = interpolate(translateY.value, inputRange, [90, 100]);
-    // const opacity = interpolate(translateY.value, inputRange, [0.9, 1]);
-    // const top = interpolate(translateY.value, inputRange, [20, 40]);
+  const cardStyles = Accounts.map((_, i) => {
+    const opacity = interpolate(translateY.value, [0, 60], [1, 0]);
+    const zIndex = i === 0 ? 3 : i === 1 ? 2 : 1;
+    const width = interpolate(translateY.value, [0, 60], [100, 80]);
 
-    return {
-      width: `${width1.value}%`,
-      opacity: opacity1.value,
-      top: top1.value,
-    };
-  });
-
-  const rStyle2 = useAnimatedStyle(() => {
-    // const width = interpolate(translateY.value, inputRange, [80, 90]);
-    // const opacity = interpolate(translateY.value, inputRange, [0.8, 0.9]);
-    // const top = interpolate(translateY.value, inputRange, [0, 20]);
-
-    return {
-      width: `${width2.value}%`,
-      opacity: opacity2.value,
-      top: top2.value,
-    };
+    return useAnimatedStyle(() => ({
+      opacity,
+      width: `${width}%`,
+      zIndex,
+    }));
   });
 
   return (
@@ -122,12 +86,12 @@ const Card = () => {
               entering={FadeInDown.springify().delay(i * 100)}
               style={[
                 styles.card,
+                cardStyles[i],
+                i === 0 && rStyle,
                 {
                   backgroundColor: acct.color,
+                  top: i * 20,
                 },
-                i === 2 && rStyle,
-                i === 1 && rStyle1,
-                i === 0 && rStyle2,
               ]}
             >
               <Text style={styles.cardTxt1}>{acct.name} Account</Text>
