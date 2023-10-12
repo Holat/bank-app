6ec041219093bbbd7bd413991bfb01aba861bded
Holat <105239299+Holat@users.noreply.github.com>
2023-10-12 08:08:35 +0100

    qucikAcess

---
+++ src/components/quickAccess.jsx
@@ -0,0 +1,106 @@
+import { StyleSheet, View } from "react-native";
+import React, { useContext } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  interpolateColor,
+  useDerivedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+import { QuickAccessIcon, QuickAccessList } from "../constants";
+import { ThemeContext } from "../constants/ThemeContextProvider";
+import { Colors } from "../constants/Theme";
+const QuickAccess = () => {
+  const { theme } = useContext(ThemeContext);
+  const progress = useDerivedValue(() => {
+    return theme === "light" ? withTiming(0) : withTiming(1);
+  }, [theme]);
+
+  const rTxtStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [1, 0],
+      [Colors.light.background, Colors.dark.background]
+    );
+
+    return {
+      color,
+    };
+  });
+  return (
+    <View
+      style={{
+        marginBottom: 15,
+        paddingHorizontal: 15,
+      }}
+    >
+      <Animated.Text
+        style={[
+          {
+            color: "white",
+            fontSize: 12,
+            marginBottom: 5,
+            paddingLeft: 5,
+            opacity: 0.5,
+          },
+          rTxtStyle,
+        ]}
+      >
+        Quick Access
+      </Animated.Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          gap: 15,
+        }}
+      >
+        {QuickAccessList.map(({ name, color }, i) => {
+          return (
+            <View
+              key={name}
+              style={{
+                borderRadius: 5,
+                backgroundColor:
+                  theme === "dark" ? Colors.dark.card : Colors.light.card,
+                paddingVertical: 12,
+                paddingHorizontal: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+
+                // elevation: 5,
+              }}
+            >
+              <QuickAccessIcon color={color} i={i} />
+              <Animated.Text
+                style={[
+                  {
+                    color: "white",
+                    fontWeight: "bold",
+                    fontSize: 14,
+                  },
+                  rTxtStyle,
+                ]}
+              >
+                {name}
+              </Animated.Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default QuickAccess;
+
+const styles = StyleSheet.create({});
