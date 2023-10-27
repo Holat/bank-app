c5983687258ad15740a6c48532cb8fb639aaca53
Holat <105239299+Holat@users.noreply.github.com>
2023-10-27 23:33:51 +0100

    new font

--- src/hooks/useCustomFonts.js
+++ src/hooks/useCustomFonts.js
@@ -4,25 +4,26 @@ import * as SplashScreen from "expo-splash-screen";
 
 // SplashScreen.preventAutoHideAsync();
 const useCustomFonts = () => {
-  const [loaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     RobotoThin: require("../../assets/fonts/Roboto-Thin.ttf"),
     RobotoLight: require("../../assets/fonts/Roboto-Light.ttf"),
     RobotoRegular: require("../../assets/fonts/Roboto-Regular.ttf"),
     RobotoBold: require("../../assets/fonts/Roboto-Bold.ttf"),
     RobotoMedium: require("../../assets/fonts/Roboto-Medium.ttf"),
     Mon: require("../../assets/fonts/Montserrat-VariableFont_wght.ttf"),
+    MonSemibold: require("../../assets/fonts/Montserrat-SemiBold.ttf"),
     MonBold: require("../../assets/fonts/Montserrat-Bold.ttf"),
     Teko: require("../../assets/fonts/Teko-VariableFont_wght.ttf"),
     Agbalumo: require("../../assets/fonts/Agbalumo-Regular.ttf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
-    if (loaded) {
+    if (fontsLoaded) {
       await SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [fontsLoaded]);
 
-  if (!loaded) return null;
+  if (!fontsLoaded) return null;
 
   return onLayoutRootView;
 };
